8551d5467a6d1f99b107e11504b12e2c2751df2c
gnanamram16 <gnanamram16@gmail.com>
2026-03-28 07:20:11 +0530

    Create ConfigReader.java

---
+++ src/main/java/config/ConfigReader.java
@@ -0,0 +1,19 @@
+package config;
+ 
+import java.util.Properties;
+ 
+public class ConfigReader {
+    private static Properties prop = new Properties();
+ 
+    static {
+        try {
+            prop.load(ConfigReader.class.getClassLoader().getResourceAsStream("config.properties"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+ 
+    public static String get(String key) {
+        return prop.getProperty(key);
+    }
+}
